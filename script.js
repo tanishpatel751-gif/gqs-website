55a5e7f3cc55187dbf1edb651343a8429c6b9355
Aryan <aryansoul6gmail.com>
2025-11-15 21:56:30 -0500

    Added image carousel.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Welcome to GQS!");
+
+
+const track = document.querySelector('.carousel-track');
+const items = Array.from(track.children);
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function updateCarousel() {
+  const itemWidth = items[0].getBoundingClientRect().width + 30; 
+  track.style.transform = `translateX(${-index * itemWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  if (index < items.length - 1) index++;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  if (index > 0) index--;
+  updateCarousel();
+});
